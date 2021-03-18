cb210694623ab2fbf62fdac60afbb763494f9647
gui-lizarza <guilherme.a.lizarzaburu@gmail.com>
2021-03-17 23:54:25 -0300

    Updated v2

--- app/routers/v2/methods.py
+++ app/routers/v2/methods.py
@@ -1,23 +1,26 @@
 import time
+import os
 
 import numpy as np
 import hnswlib
 
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
 from typing import List
 
 router = APIRouter()
 
-data = np.random.normal(loc=0.0, scale=1.0, size=(1000, 3))
-
-item_count = len(data)
-data_labels = np.arange(item_count)
-
 graph = hnswlib.Index(space='cosine', dim=3)
-graph.init_index(max_elements=len(data)*2, ef_construction=200, M=16)
-graph.add_items(data, data_labels)
-graph.set_ef(50)
+
+if os.path.isfile('app/graph.bin'):
+    graph.load_index('app/graph.bin')
+else:
+    graph.init_index(max_elements=1000, ef_construction=200, M=50)
+    graph.set_ef(50)
+
+class QueryRequest(BaseModel):
+    vec: List[float]
+    k: int
 
 class QueryResponse(BaseModel):
     time_passed: float
@@ -28,12 +31,22 @@ class AddResponse(BaseModel):
     time_passed: float
     item_count: int
 
+class SaveResponse(BaseModel):
+    time_passed: float
+    message: str
+
 @router.put('/query', response_model=QueryResponse)
-async def make_query(vec: List[float]):
+async def make_query(request: QueryRequest):
     
-    tic = time.time()
-    labels, distances = graph.knn_query(vec, k=3)
-    toc = time.time()
+    vec = request.vec
+    k = request.k
+    
+    try:
+        tic = time.time()
+        labels, distances = graph.knn_query(vec, k=k)
+        toc = time.time()
+    except RuntimeError:
+        raise HTTPException(status_code=400, detail="Menos do que {} nós no grafo".format(k))
     
     labels = labels.squeeze(axis=0)
     distances = distances.squeeze(axis=0)
@@ -49,17 +62,29 @@ async def make_query(vec: List[float]):
 @router.put('/add', response_model=AddResponse)
 async def add_item(vec: List[float]):
 
-    global item_count
-    curr_count = item_count
-
+    label = graph.get_current_count()
     tic = time.time()
-    graph.add_items(vec, item_count)
+    graph.add_items(vec, label)
     toc = time.time()
 
-    item_count += 1
+    time_passed = toc - tic
+
+    item_count = label + 1
+    return {
+        'time_passed': time_passed,
+        'item_count': item_count
+    }
+
+@router.put('/save', response_model=SaveResponse)
+async def save_index():
+
+    tic = time.time()
+    graph.save_index('app/graph.bin')
+    toc = time.time()
+
     time_passed = toc - tic
 
     return {
         'time_passed': time_passed,
-        'item_count': curr_count
+        'message': 'Index saved!'
     }
